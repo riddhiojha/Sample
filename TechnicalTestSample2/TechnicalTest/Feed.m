405f54790b28b2d81f94e0c11fd00e7c2eb510d7
RIDDHI OJHA <riddhi.ojha@gmail.com>
2014-09-06 00:09:15 +0400

    Final Changes

---
+++ TechnicalTestSample2/TechnicalTest/Feed.m
@@ -0,0 +1,13 @@
+//
+//  Feed.m
+//  TechnicalTest
+//
+//  Created by Mobii_Mac on 9/5/14.
+//  Copyright (c) 2014 Mobii_Mac. All rights reserved.
+//
+
+#import "Feed.h"
+
+@implementation Feed
+
+@end
---
+++ TechnicalTestSample2/TechnicalTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TechnicalTest
+//
+//  Created by Mobii_Mac on 9/5/14.
+//  Copyright (c) 2014 Mobii_Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
